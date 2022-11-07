52811c20c4358fac39bef20ea666c9904a5bb9a3
eduardo <eduardov.argenton@outlook.com>
2022-11-07 12:24:22 -0300

    Login Page + Logout

--- Library/src/main/java/br/com/arqsoft/Library/config/security/WebSecurityConfig.java
+++ Library/src/main/java/br/com/arqsoft/Library/config/security/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -15,12 +16,18 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .httpBasic()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/books", true)
                 .and()
                 .authorizeHttpRequests()
                 .anyRequest().authenticated()
                 .and()
-                .csrf().disable();
+                .csrf().disable()
+                .logout().clearAuthentication(true).logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/test")
+                .deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true);
         return http.build();
     }
 
--- Library/src/main/java/br/com/arqsoft/Library/controllers/DefaultPageController.java
+++ Library/src/main/java/br/com/arqsoft/Library/controllers/DefaultPageController.java
@@ -3,11 +3,9 @@ package br.com.arqsoft.Library.controllers;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value = "")
 public class DefaultPageController {
 
     @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
@@ -15,4 +13,11 @@ public class DefaultPageController {
     public ModelAndView default_page() {
         return new ModelAndView("redirect:/books");
     }
+
+    @GetMapping("/login")
+    public ModelAndView login(){
+        return new ModelAndView("users/login");
+    }
 }
+
+
